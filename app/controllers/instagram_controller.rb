c4d8bc90408618a269e9bd3d6a953266bab91ac7
Nicholas Sellek <nick@seansellek.com>
2018-03-02 11:15:32 -0500

    changed user_authentication works

--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -1,11 +1,13 @@
 class InstagramController < ApplicationController
+  before_action :get_user, except: :authorize
 
   def authorize
     token = JwtToken.find_by(jwt_token: params['jwt'])
     user = token.user || User.find_or_initialize_by(instagram_code: params[:code])
 
-    if user.new_record? || user.instagram_token.nil?
+    if user.new_record? || user.instagram_token.nil? || user.instagram_code != params['code']
       access_token = @@instagram.auth_token(params[:code])
+      user.instagram_code = params[:code]
       user.instagram_token = access_token['access_token']
       user.username = access_token['user']['username']
       user.save!
