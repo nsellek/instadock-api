856d36cea73ad72ca3a5f8d1725ba66c2095ca6e
Nicholas Sellek <nick@seansellek.com>
2018-03-03 12:23:17 -0500

    fixed app authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,17 @@ class ApplicationController < ActionController::API
   INSTAGRAM = Instagram.new
 
   def authorize_app
-    render json: {code: 400, error: 'JWT required'}, status: 400 && return unless params[:jwt].present?
+    unless params[:jwt].present?
+      render json: {code: 400, error: 'JWT required'}, status: 400
+      return false
+    end
 
     decoded_token = JwtToken.decode(params[:jwt])
 
-    render json: {code: 401, error: 'Invalid token'}, status: 401 unless decoded_token[0]['app_secret'] == ENV['APP_SECRET']
+    unless decoded_token[0]['app_secret'] == ENV['APP_SECRET']
+      render json: {code: 401, error: 'Invalid token'}, status: 401
+      return false
+    end
   end
 
   def get_user
